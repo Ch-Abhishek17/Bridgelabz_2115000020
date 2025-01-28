8c9884816cd300b9ae891889d264ceb079e1781f
Ch-Abhishek17 <abhimanyu17abd@gmail.com>
2025-01-28 22:03:02 +0530

    Added codes to Feature_28-Jan-2025

---
+++ ChocolateDivision.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ChocolateDivision {
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        int[] result = new int[2];
+        result[0] = number / divisor;
+        result[1] = number % divisor;
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of chocolates: ");
+        int numberOfChocolates = sc.nextInt();
+        
+        System.out.print("Enter the number of children: ");
+        int numberOfChildren = sc.nextInt();
+        
+        int[] result = findRemainderAndQuotient(numberOfChocolates, numberOfChildren);
+        
+        System.out.println("Each child will get: " + result[0] + " chocolates");
+        System.out.println("Remaining chocolates: " + result[1]);
+        
+    }
+}
